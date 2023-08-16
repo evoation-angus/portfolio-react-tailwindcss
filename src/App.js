93962ddd011d7bf10b2130f9195a2e882ad65c32
Angus Horan <angus.horan@gmail.com>
2023-08-16 22:01:27 +1200

    url changes

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ function App() {
         <BrowserRouter>
           <Navbar />
           <Routes>
-            <Route element={<Home/>} path="/portfolio-react-tailwindcss" ></Route>
-            <Route element={<Playground/>} path="/portfolio-react-tailwindcss/playground" ></Route>
-            <Route element={<Projects/>} path="/portfolio-react-tailwindcss/projects" ></Route>
-            <Route element={<Courses/>} path="/portfolio-react-tailwindcss/courses" ></Route>
-            <Route element={<AboutMe/>} path="/portfolio-react-tailwindcss/about-me" ></Route>
+            <Route element={<Home/>} path="/" ></Route>
+            <Route element={<Playground/>} path="playground" ></Route>
+            <Route element={<Projects/>} path="/projects" ></Route>
+            <Route element={<Courses/>} path="/courses" ></Route>
+            <Route element={<AboutMe/>} path="about-me" ></Route>
           </Routes>
         </BrowserRouter>
       </div>
--- src/Nav.js
+++ src/Nav.js
@@ -29,23 +29,23 @@ export default function Navbar() {
   return (
     <nav className="bg-slate-50 dark:bg-neutral-900 py-4 px-6 text-sm font-medium fixed z-10 w-full ">
       <ul className="hidden md:flex space-x-3 justify-center">
-        <NavLink id="home" to="/portfolio-react-tailwindcss"  className={({isActive}) => [className, isActive ? "!bg-green-500" : null, ].filter(Boolean).join(" ")}>
+        <NavLink id="home" to="/"  className={({isActive}) => [className, isActive ? "!bg-green-500" : null, ].filter(Boolean).join(" ")}>
           <img className="h-5 icon dark:invert mr-1" src={houseicon} alt="" />
           <span className="text-black dark:text-white">Home</span>
         </NavLink>
-        <NavLink id="play" to="/portfolio-react-tailwindcss/playground"  className={({isActive}) => [className, isActive ? "!bg-green-500" : null, ].filter(Boolean).join(" ")}>
+        <NavLink id="play" to="/playground"  className={({isActive}) => [className, isActive ? "!bg-green-500" : null, ].filter(Boolean).join(" ")}>
           <img className="h-5 icon dark:invert mr-1" src={playicon} alt="" />
           <span className="text-black dark:text-white">Playground</span>
         </NavLink>
-        <NavLink id="projects" to="/portfolio-react-tailwindcss/projects"  className={({isActive}) => [className, isActive ? "!bg-green-500" : null, ].filter(Boolean).join(" ")}>
+        <NavLink id="projects" to="/projects"  className={({isActive}) => [className, isActive ? "!bg-green-500" : null, ].filter(Boolean).join(" ")}>
           <img className="h-5 icon dark:invert mr-1" src={projecticon} alt="" />
           <span className="text-black dark:text-white">Projects</span>
         </NavLink>
-        <NavLink id="courses" to="/portfolio-react-tailwindcss/courses"  className={({isActive}) => [className, isActive ? "!bg-green-500" : null, ].filter(Boolean).join(" ")}>
+        <NavLink id="courses" to="/courses"  className={({isActive}) => [className, isActive ? "!bg-green-500" : null, ].filter(Boolean).join(" ")}>
           <img className="h-5 icon dark:invert mr-1" src={coursesicon} alt="" />
           <span className="text-black dark:text-white">Courses</span>
         </NavLink>
-        <NavLink id="about-me" to="/portfolio-react-tailwindcss/about-me"  className={({isActive}) => [className, isActive ? "!bg-green-500" : null, ].filter(Boolean).join(" ")}>
+        <NavLink id="about-me" to="/about-me"  className={({isActive}) => [className, isActive ? "!bg-green-500" : null, ].filter(Boolean).join(" ")}>
           <img className="h-5 icon dark:invert mr-1" src={usericon} alt="" />
           <span className="text-black dark:text-white">About me</span>
         </NavLink>
@@ -80,7 +80,7 @@ export default function Navbar() {
         <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
           <li>
             <NavLink 
-              to="/portfolio-react-tailwindcss"
+              to="/"
               className="text-3xl block py-5 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
               aria-current="page"
               onClick={toggleVisibility}
@@ -90,7 +90,7 @@ export default function Navbar() {
           </li>
           <li>
             <NavLink
-              to="/portfolio-react-tailwindcss/playground"
+              to="/playground"
               className="text-3xl block py-5 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
               onClick={toggleVisibility}
             >
@@ -99,7 +99,7 @@ export default function Navbar() {
           </li>
           <li>
             <NavLink
-              to="/portfolio-react-tailwindcss/projects"
+              to="/projects"
               className="text-3xl block py-5 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white"
               onClick={toggleVisibility}
             >
@@ -108,7 +108,7 @@ export default function Navbar() {
           </li>
           <li>
             <NavLink
-              to="/portfolio-react-tailwindcss/courses"
+              to="/courses"
               className="text-3xl block py-5 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
               onClick={toggleVisibility}
             >
@@ -117,7 +117,7 @@ export default function Navbar() {
           </li>
           <li>
             <NavLink
-              to="/portfolio-react-tailwindcss/about-me"
+              to="/about-me"
               className="text-3xl block py-5 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
               onClick={toggleVisibility}
             >
